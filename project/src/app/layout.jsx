c96fb9beca052ee004e3b08a5d343f66e77bc0f1
reysree <sreeram.bangaroo@gmail.com>
2025-02-03 17:06:07 -0500

    Added google analytics to portfolio

--- project/src/app/layout.jsx
+++ project/src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "../index.css";
 
 export const metadata = {
@@ -8,6 +9,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-P5X8B2GMKP"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-P5X8B2GMKP');
+            `,
+          }}
+        />
+      </head>
       <body>
         <div id="root">{children}</div>
       </body>
